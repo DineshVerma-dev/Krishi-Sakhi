4df240d00c430bf88eefa8eb99e6d8b1bcf138aa
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-09-18 00:09:37 +0530

    Landing Page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
 import './App.css'
+import Hero from  './Components/Hero'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
-      <h1 className='bg-green-500 text-5xl text-center'>Krishi-Sakhi</h1>
+      <Hero/>
     </>
   )
 }
